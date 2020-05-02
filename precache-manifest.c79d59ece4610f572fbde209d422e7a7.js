828001fb4110ce5c630852b304730cb2e05a49d9
gisiora <james.nyakundi@yahoo.com>
2020-05-02 16:08:25 +0100

    Updates

---
+++ precache-manifest.c79d59ece4610f572fbde209d422e7a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c25e94d7a68e5802aae743191cafc2e",
+    "url": "/robot/index.html"
+  },
+  {
+    "revision": "c99d6235fc0f61408166",
+    "url": "/robot/static/css/main.0b9df4c7.chunk.css"
+  },
+  {
+    "revision": "4473620448303fb630fc",
+    "url": "/robot/static/js/2.4fe4915c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot/static/js/2.4fe4915c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99d6235fc0f61408166",
+    "url": "/robot/static/js/main.6c9fd779.chunk.js"
+  },
+  {
+    "revision": "389a0a16ab903092237b",
+    "url": "/robot/static/js/runtime-main.5a058cd6.js"
+  }
+]);
